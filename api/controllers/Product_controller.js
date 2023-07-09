5927d229c97098299db4e2baf6039190fcb9685d
MEHDI BELLAM <porcepic91@gmail.com>
2023-07-10 00:21:48 +0200

    avancement et ajout de route pour supprimer un produits et l'update

--- api/controllers/Product_controller.js
+++ api/controllers/Product_controller.js
@@ -18,5 +18,40 @@ router.create_product = async (req, res) => {
   await newProduct.save();
   return res.status(200).send(newProduct);
 };
+router.delete_product = async (req, res) => {
+  const deleteProduct = await product.deleteOne({ name: req.query.name });
+  return res.status(200).send(deleteProduct);
+};
+router.update_product = async (req, res) => {
+  try {
+    const updatedProduct = await product.updateOne(
+      { name: req.query.name },
+      {
+        $set: {
+          name: req.body.name,
+          type: req.body.type,
+          price: req.body.price,
+          rating: req.body.rating,
+          available: req.body.available,
+        },
+      }
+    );
+    console.log(updatedProduct);
+
+    if (updatedProduct.nModified === 0) {
+      return res.status(404).json({ message: "Product not found." });
+    }
+
+    return res.status(200).json({ message: "Product updated successfully.", });
+    
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ message: "An error occurred while updating the product." });
+  }
+};
+
+
 
 export default router;
--- api/routes/product_routes.js
+++ api/routes/product_routes.js
@@ -5,4 +5,6 @@ import product_controller from '../controllers/Product_controller.js';
 router.get("/", product_controller.get_all_products);
 router.get("/name", product_controller.get_one_product);
 router.post("/", product_controller.create_product);
+router.delete("/name", product_controller.delete_product);
+router.put("/name", product_controller.update_product);
 export default router;
