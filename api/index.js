c3fcabc77c2d8e5bd40e95ec6adb8e2e84c43acc
MEHDI BELLAM <porcepic91@gmail.com>
2023-07-09 09:52:05 +0200

    :package: ajout des package nécéssaire :package:

---
+++ api/index.js
@@ -0,0 +1,6 @@
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const { Server } = require("socket.io");
+require("dotenv").config();
+const mongoose = require("mongoose");
