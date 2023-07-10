8426b13da3f054b4e8d9d1fc0685a94313eac611
MEHDI BELLAM <porcepic91@gmail.com>
2023-07-10 08:43:16 +0200

    :bug: correction de la route update  :bug:

--- api/controllers/Product_controller.js
+++ api/controllers/Product_controller.js
@@ -28,21 +28,33 @@ router.update_product = async (req, res) => {
       { name: req.query.name },
       {
         $set: {
-          name: req.body.name,
-          type: req.body.type,
-          price: req.body.price,
-          rating: req.body.rating,
-          available: req.body.available,
+          name: req.query.name,
+          type: req.query.type,
+          price: req.query.price,
+          rating: req.query.rating,
+          available: req.query.available,
         },
       }
     );
-    console.log(updatedProduct);
+    console.log( req.query.name, req.query.type,
+ req.query.price,
+   req.query.rating,
+         req.query.available,);
 
     if (updatedProduct.nModified === 0) {
       return res.status(404).json({ message: "Product not found." });
     }
 
-    return res.status(200).json({ message: "Product updated successfully.", });
+    return res
+      .status(200)
+      .json({
+        message: "Product updated successfully.",
+        name: req.body.name,
+        type: req.body.type,
+        price: req.body.price,
+        rating: req.body.rating,
+        available: req.body.available,
+      });
     
   } catch (error) {
     console.error(error);
